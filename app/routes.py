cd15ff3f0ed71efe898d6c143334dbce1ddc3496
Cyb3rFake <74620864+Cyb3rFake@users.noreply.github.com>
2024-01-10 00:43:57 +0300

    upd

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,19 @@
 from app import app
+
 from flask import render_template, request
 import requests
 from bs4 import BeautifulSoup
 
-
 @app.route('/')
 def index() -> str:
-    return app.render_template('index.html')
+
+    return render_template('index.html')
 
  
 @app.route('/parse', methods=['POST'])
 def parse() -> str:
     # Заготовка для парсера контента сайта
-    url = app.request.form['url']
+    url = request.form['url']
 
     response = requests.get(url)
 
@@ -21,4 +22,5 @@ def parse() -> str:
     # здесь типа должен быть код самого парсера, результат которого сохраняется в список headlines
     # который выводиться в шаблон results.html
         
-    return app.render_template('results.html', headlines=headlines)
+    return render_template('results.html', headlines=headlines)
+
